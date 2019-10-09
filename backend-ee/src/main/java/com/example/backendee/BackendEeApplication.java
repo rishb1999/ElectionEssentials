fe135d816cb1cd0b9186dbe9c41f731a6344f12c
jackweakley <jackweakley1@gmail.com>
2019-10-08 20:25:03 -0500

    backend skeleton

---
+++ backend-ee/src/main/java/com/example/backendee/BackendEeApplication.java
@@ -0,0 +1,13 @@
+package com.example.backendee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendEeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendEeApplication.class, args);
+	}
+
+}
