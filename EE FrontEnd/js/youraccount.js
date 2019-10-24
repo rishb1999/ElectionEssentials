a46292e0bb5e698e2b5f20db2b6b311fbd493170
jackweakley <jackweakley1@gmail.com>
2019-10-23 22:31:21 -0500

    commit

--- EE FrontEnd/js/youraccount.js
+++ EE FrontEnd/js/youraccount.js
@@ -8,7 +8,9 @@ function sendUserLoginDetails(){
     var password_ = document.getElementById("password").value;
     var confirm_Password = document.getElementById("confirmPassword").value;
 
-    var URL = "something.com";
+    var baseURL = "something.com";
+    var query = "/users/";
+    var URL = baseURL + query;
 
     var data = {
         firstName : first_Name,
@@ -20,6 +22,25 @@ function sendUserLoginDetails(){
         password : password_,
         confirmPassword : confirm_Password
     };
+
+    $.post(URL, data)
+        .fail(function() {
+            alert("Please try again.");
+        });
+}
+
+function verifyLoginSuccess(){
+    var username_ = document.getElementById("username").value;
+    var password_ = document.getElementById("password").value;
+
+    var baseURL = "something.com";
+    var query = "idk";
+    var URL = baseURL + query;
+
+    var data = {
+        username : username_,
+        password : password_
+    }
 
     $.post(URL, data)
         .fail(function() {
