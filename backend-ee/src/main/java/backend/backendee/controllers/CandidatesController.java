6aea327b1b35d78e2f4f8623c2f0708a677d60c6
cristianascencio <cascencio@utexas.edu>
2019-10-25 23:31:23 -0500

    added cors annotation

--- backend-ee/src/main/java/backend/backendee/controllers/CandidatesController.java
+++ backend-ee/src/main/java/backend/backendee/controllers/CandidatesController.java
@@ -4,14 +4,12 @@ import backend.backendee.models.Candidates;
 import backend.backendee.repositories.CandidatesRepository;
 import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
 import javax.validation.Valid;
 import java.util.List;
 
+@CrossOrigin
 @RestController
 @RequestMapping(value = "/candidates")
 public class CandidatesController {
--- backend-ee/src/main/java/backend/backendee/controllers/UsersController.java
+++ backend-ee/src/main/java/backend/backendee/controllers/UsersController.java
@@ -4,16 +4,13 @@ import backend.backendee.models.Users;
 import backend.backendee.repositories.UsersRepository;
 import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
 import javax.validation.Valid;
 import java.util.HashMap;
 import java.util.List;
 
-
+@CrossOrigin
 @RestController
 @RequestMapping(value = "/users")
 public class UsersController {
